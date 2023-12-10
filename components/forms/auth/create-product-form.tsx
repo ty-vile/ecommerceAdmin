6b21c7459d0f335f53ead5022f507b45d920995d
Ty <typhn.sa@gmail.com>
2023-12-10 20:56:06 +1030

    handle uploading and previewing images for product (frontend only)

--- components/forms/auth/create-product-form.tsx
+++ components/forms/auth/create-product-form.tsx
@@ -19,13 +19,12 @@ import { Button } from "../../ui/button";
 // hooks
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 // toast
 import { toast } from "react-toastify";
+// icons
 import { FaPlus } from "react-icons/fa";
-
-// const MAX_FILE_SIZE = 500000000000000000000;
-// const ACCEPTED_IMAGE_TYPES = ["image/jpg", "image/png", "image/png"];
+import Image from "next/image";
 
 const formSchema = z.object({
   name: z
@@ -37,16 +36,12 @@ const formSchema = z.object({
     .min(10, { message: "Product description must be at least 10 characters" })
     .max(200, "Product description must be less than 200 characters"),
   image: z.any(),
-  // .refine((file) => file?.size <= MAX_FILE_SIZE, "Max file size is 5mb")
-  // .refine(
-  //   (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-  //   "Only .jpg, .jpeg, .png files are supported"
-  // ),
 });
 
 const CreateProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [files, setFiles] = useState<FileList | null>(null);
+  const [files, setFiles] = useState<FileList | undefined>(undefined);
+  const [filesUrl, setFileUrls] = useState<String[] | []>([]);
   const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -63,19 +58,37 @@ const CreateProductForm = () => {
     setIsLoading(false);
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFiles(e.target.files);
+  useEffect(() => {
+    if (filesUrl.length > 0) {
+      setFileUrls([]);
+    }
 
-    console.log("FILES_ALL", e.target.files);
-    console.log("FILE_1", e.target.files?.[0]);
-    console.log("FILE_2", e.target.files?.[1]);
+    if (files) {
+      const fileUrlArr = [];
+
+      for (let i = 0; i < files.length; i++) {
+        const url = URL.createObjectURL(files[i]);
+
+        fileUrlArr.push(url);
+      }
+
+      setFileUrls(fileUrlArr);
+    }
+  }, [files]);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const localFiles = e.target.files;
+
+    if (localFiles) {
+      setFiles(localFiles);
+    }
   };
 
   return (
     <section className="flex flex-col gap-4">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          {/* <FormField
+          <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
@@ -109,13 +122,13 @@ const CreateProductForm = () => {
                 <FormMessage />
               </FormItem>
             )}
-          /> */}
+          />
           <FormField
             control={form.control}
             name="image"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Product Image</FormLabel>
+                <FormLabel>Select Image(s)</FormLabel>
                 <FormControl>
                   <Input
                     multiple={true}
@@ -124,19 +137,42 @@ const CreateProductForm = () => {
                     disabled={isLoading}
                     {...field}
                     onChange={handleFileChange}
+                    accept=".jpg, .jpeg, .png"
                   />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-
+          {filesUrl.length > 0 && (
+            <div className="pt-2">
+              <FormLabel>Product Image{filesUrl.length > 1 && "s"}</FormLabel>
+            </div>
+          )}
+          <div className="flex items-center gap-4 overflow-x-scroll">
+            {filesUrl.length > 0 &&
+              filesUrl.map((file, i) => {
+                return (
+                  <>
+                    <Image
+                      src={file as string}
+                      alt="Product Image"
+                      height={0}
+                      width={0}
+                      className="h-auto w-auto max-h-64"
+                      key={i}
+                    />
+                  </>
+                );
+              })}
+          </div>
           <div>
             <Button
               className={`flex items-center gap-2 bg-green-600 hover:bg-green-700 transition-300 w-full ${
                 isLoading && "bg-gray-100/70"
               }`}
               disabled={isLoading}
+              onClick={() => console.log(files)}
             >
               <FaPlus />
               {isLoading ? "Creating Product..." : "Create Product"}
